85ad57c4d912608bcf2da5e6ce00713ca05bd05b
alexmillieret <alex.millieret@gmail.com>
2018-04-05 14:39:26 +0200

    creation de classe

---
+++ gestibankProject/src/beans/Client.java
@@ -0,0 +1,18 @@
+package beans;
+
+import java.util.ArrayList;
+
+public class Client extends Utilisateur {
+private ArrayList<Compte> comptes;
+private int idConseiller;
+
+public Client() {
+	super();
+	this.comptes=new ArrayList<Compte>;
+	this.idConseiller=0;
+	// TODO Auto-generated constructor stub
+}
+
+
+
+}
---
+++ gestibankProject/src/beans/Conseiller.java
@@ -0,0 +1,17 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Conseiller extends Utilisateur {
+private ArrayList<Client> listeClient;
+private Date dateDebutContrat;
+	public Conseiller(String nomUtilisateur, String password, String nom, String prenom, String mail, Adresse adresse,
+			String telephone, int nbEnfts) {
+		super(nomUtilisateur, password, nom, prenom, mail, adresse, telephone, nbEnfts);
+		this.listeClient=new ArrayList<Client>();
+		this.dateDebutContrat=new Date();
+		// TODO Auto-generated constructor stub
+	}
+
+}
