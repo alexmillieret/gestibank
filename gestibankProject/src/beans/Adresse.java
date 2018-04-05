c3507ed39d2c81595994a27ece4c5595d09feec7
alexmillieret <alex.millieret@gmail.com>
2018-04-05 13:49:16 +0200

    creation classe adresse

---
+++ gestibankProject/src/beans/Adresse.java
@@ -0,0 +1,65 @@
+package beans;
+
+import java.io.Serializable;
+
+public class Adresse implements Serializable{
+private Integer numero;
+private String rue;
+private String ville;
+private String pays;
+private String codePostal;
+
+public Adresse() {}
+
+public Adresse(Integer numero, String rue, String ville, String pays, String codePostal) {
+	super();
+	this.numero = numero;
+	this.rue = rue;
+	this.ville = ville;
+	this.pays = pays;
+	this.codePostal = codePostal;
+}
+
+public Integer getNumero() {
+	return numero;
+}
+
+public void setNumero(Integer numero) {
+	this.numero = numero;
+}
+
+public String getRue() {
+	return rue;
+}
+
+public void setRue(String rue) {
+	this.rue = rue;
+}
+
+public String getVille() {
+	return ville;
+}
+
+public void setVille(String ville) {
+	this.ville = ville;
+}
+
+public String getPays() {
+	return pays;
+}
+
+public void setPays(String pays) {
+	this.pays = pays;
+}
+
+public String getCodePostal() {
+	return codePostal;
+}
+
+public void setCodePostal(String codePostal) {
+	this.codePostal = codePostal;
+}
+
+
+
+}
